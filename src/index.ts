3cb03353a8e64e0f176c49d62bf166e2192d456a
Alex Rodriguez <alex.rodriguez@justtrade.com>
2022-11-07 13:01:08 +0100

    Added BullMQ and Bull Arena setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-console.log("Hello BullMQ Docker Arena")
+import Arena from 'bull-arena'
+import { Queue, FlowProducer } from 'bullmq'
+import path from 'path'
+
+const bootstrap = async () => {
+  const configPath = path.resolve('arena.json')
+  const config = require(configPath);
+  const port = Number(process.env.PORT) || 3000
+
+  Arena({
+    BullMQ: Queue,
+    FlowBullMQ: FlowProducer,
+    ...config
+  } ,{
+    basePath: '/arena',
+    port,
+  })
+
+  console.log(`BullMQ Arena is running on http://localhost:${port}/arena`)
+}
+
+bootstrap().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
